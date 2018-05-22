6cefcf7f123db45f407948663f07272eabfb0cd0
nlemay <nlemay@godaddy.com>
2018-05-21 18:49:51 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 from pip.req import parse_requirements
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements('requirements.txt', session=False)
 testing_reqs = parse_requirements('test_requirements.txt', session=False)
 
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 test_reqs = [str(ir.req) for ir in testing_reqs]
 
 
 setup(name='hermes',
       version='1.0',
-      description='Provides a hermes library interface for interacting with the OCM system',
+      description='Hermes provides an interface for interacting with GoDaddy messaging systems',
       author='DCU',
       author_email='dcueng@godaddy.com',
       url='https://github.secureserver.net/ITSecurity/hermes',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
       install_requires=reqs,
       tests_require=test_reqs,
       test_suite="nose.collector"
