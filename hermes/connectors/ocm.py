9c7976b2fc1236076cb9b3ae7be0a72b73abd26d
nwade-godaddy <80975008+nwade-godaddy@users.noreply.github.com>
2023-01-30 14:28:40 -0600

    Authenticate with cert JWTs instead of client certificates (#14)
    * Authenticate with cert JWTs instead of client certificates
    
    * Switch get to post

--- hermes/connectors/ocm.py
+++ hermes/connectors/ocm.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import requests
 
@@ -14,54 +15,52 @@ class OCMClient:
             raise UnsupportedEnvironmentException("Unsupported environment {}".format(env))
 
         self._cert = cert
+        self._jwt = None
 
         if env == 'prod':
             self.non_shopper_endpoint = 'https://messaging.api.int.godaddy.com/v1/messaging/messages/sendNonShopper'
             self.shopper_endpoint = 'https://messaging.api.int.godaddy.com/v1/messaging/messages'
+            self._sso_endpoint = 'https://sso.gdcorp.tools/v1/secure/api/token'
         elif env == 'test':
             self.non_shopper_endpoint = 'https://messaging.api.int.test-godaddy.com/v1/messaging/messages/sendNonShopper'
             self.shopper_endpoint = 'https://messaging.api.int.test-godaddy.com/v1/messaging/messages'
+            self._sso_endpoint = 'https://sso.test-gdcorp.tools/v1/secure/api/token'
         else:
             self.non_shopper_endpoint = 'https://messaging.api.int.dev-godaddy.com/v1/messaging/messages/sendNonShopper'
             self.shopper_endpoint = 'https://messaging.api.int.dev-godaddy.com/v1/messaging/messages'
+            self._sso_endpoint = 'https://sso.dev-gdcorp.tools/v1/secure/api/token'
 
     def send_shopper_email(self, email_params):
-        headers = {'Accept': 'application/json',
-                   'X-Shopper-Id': email_params.get('substitutionValues').get('ACCOUNT_NUMBER')}
-
-        response = requests.post(self.shopper_endpoint,
-                                 headers=headers,
-                                 json=email_params,
-                                 cert=self._cert)
-
-        response.raise_for_status()
-        return json.loads(response.text)
+        headers = {'Accept': 'application/json', 'X-Shopper-Id': email_params.get('substitutionValues').get('ACCOUNT_NUMBER')}
+        return self._make_request(self.shopper_endpoint, headers, email_params)
 
     def send_non_shopper_email(self, email_params):
-        headers = {'Accept': 'application/json',
-                   'X-Private-Label-Id': '1'}
-
-        response = requests.post(self.non_shopper_endpoint,
-                                 headers=headers,
-                                 json=email_params,
-                                 cert=self._cert)
-
-        response.raise_for_status()
-        return json.loads(response.text)
+        headers = {'Accept': 'application/json', 'X-Private-Label-Id': '1'}
+        return self._make_request(self.non_shopper_endpoint, headers, email_params)
 
     def get_status(self, message_id):
         assert isinstance(message_id, str)
-
-        headers = {'Accept': 'application/json',
-                   'Content-Type': 'application/json'}
-
-        response = requests.get(self.shopper_endpoint + '/' + message_id,
-                                headers=headers,
-                                cert=self._cert)
-
-        response.raise_for_status()
-        resp_dict = json.loads(response.text)
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        resp_dict = self._make_request(self.shopper_endpoint + '/' + message_id, headers, None)
 
         if resp_dict.get('status') not in self.success_status:
             raise OCMException(resp_dict.get('failureReason'))
         return resp_dict.get('status')
+
+    def _make_request(self, url: str, headers: dict, body: Optional[dict]) -> dict:
+        headers.update({'Authorization': f'sso-jwt {self._get_jwt(False)}'})
+        response = requests.post(url, headers=headers, json=body)
+        if response.status_code in [401, 403]:
+            headers.update({'Authorization': f'sso-jwt {self._get_jwt(True)}'})
+        response.raise_for_status()
+        return json.loads(response.text)
+
+    def _get_jwt(self, force: bool) -> Optional[str]:
+        if not self._jwt or force:
+            try:
+                response = requests.post(self._sso_endpoint, data={'realm': 'cert'}, cert=self._cert)
+                response.raise_for_status()
+                return json.loads(response.text).get('data')
+            except Exception:
+                pass
+        return self._jwt
