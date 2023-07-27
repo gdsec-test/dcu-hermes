1fba57bdec33a0ee96364ef1298a87d1e8cfe6c0
ntitus <ntitus@godaddy.com>
2023-07-27 11:10:12 -0400

    bumping up version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md') as f:
 
 setup(
     name='hermes',
-    version='1.2.2',
+    version='1.2.3',
     author='DCU',
     author_email='dcueng@godaddy.com',
     description='Hermes provides an interface for interacting with GoDaddy messaging systems',
